09c05748e57cc524f2a04093438073e51ba0ffbf
Short <57575118+Luppyn@users.noreply.github.com>
2024-05-27 16:46:15 -0300

    Update funcoes.py
    Apenas aplicando as regras do pylint de acordo com o que não está desabilitado:
    
          C0114, #missing-module-docstring
          C0115, #missing-class-docstring
          C0116, #missing-function-docstring
          C0209, #consider-using-f-string
          C0301, #line-too-long
          C0303, #trailing-whitespace     
          C0304, #missing-final-newline
          C0305, #trailing-newlines
          C0321, #multiple-statements
          R0903, #too-few-public-methods
          W0601, #global-variable-undefined
          W0612, #unused-variable
          W0718, #broad-exception-caught
          W0719, #broad-exception-raised

--- funcoes.py
+++ funcoes.py
@@ -1,14 +1,13 @@
-from pywinauto import mouse, Application as app
-import pydirectinput as pdi # pip install pydirectinput-rgx
 from time import sleep
 import datetime
+from pywinauto import mouse, Application as app
+import pydirectinput as pdi # pip install pydirectinput-rgx
 from joblib import Parallel, delayed
-import random
 import keyboard
 
 
 # Building the functions
-def detectingWindow(maximize_window=True):
+def detecting_window(maximize_window=True):
     try:
         program = app().connect(title='Roblox', found_index=0)
         
@@ -24,8 +23,8 @@ def detectingWindow(maximize_window=True):
 
         while roblox_window.exists(timeout=10):
             return True
-    except:
-        return None
+    except Exception as exc:
+        return exc
 
 
 def walk(direction='w', walk_duration=1.0, _pause=False):
@@ -41,11 +40,11 @@ def sprintwalk(sprint_key='ctrl', sprint_toggle=True, two_keys=False, direction=
         
         try:
             walk(direction[0], walk_duration)
-        except:
-            raise 'Invalid Direction'
+        except Exception as exc:
+            raise exc
         
         pdi.press(sprint_key)
-    elif two_keys == True:
+    elif two_keys:
         pdi.press(sprint_key, interval=_interval, _pause=False)
         walk(sprint_key, walk_duration,  _pause=False)
     else:
@@ -54,28 +53,28 @@ def sprintwalk(sprint_key='ctrl', sprint_toggle=True, two_keys=False, direction=
         
         try:
             walk(direction[0], walk_duration)
-        except:
-            raise "Invalid Direction"
+        except Exception as exc:
+            raise exc
         pdi.keyUp(sprint_key)
 
 
-def selectItem(item_position:list, delay_between=0.25):
+def select_item(item_position:list, delay_between=0.25):
     for item in item_position:
         pdi.write(f'{item}', interval=delay_between)
 
 
-def useItem(button=('left', 'middle', 'right'), quantity=1, x=None, y=None, delay_in_use=0.0):
-    pdi.click(button=button[0],clicks=quantity, x=x, y=y, interval=delay_in_use)
+def use_item(button=('left', 'middle', 'right'), quantity=1, x=None, y=None, delay_in_use=0.0):
+    pdi.click(button=button,clicks=quantity, x=x, y=y, interval=delay_in_use)
 
 
-def pressKey(keys:list, delay_to_start=0.0, delay_between=0.09):
+def press_key(keys:list, delay_to_start=0.0, delay_between=0.09):
     for key in keys:
         sleep(delay_to_start)
         pdi.press(keys=key, duration=delay_between)
 
 
 # Sometimes can not work properly
-def openInventory():
+def open_inventory():
     x = roblox_window_info.right - 30
     y = roblox_window_info.top + 40
 
@@ -91,13 +90,13 @@ def openInventory():
     pdi.click(clicks=1)
 
 
-def controlInventory(_from:list, action=('Put','Take')):
+def control_inventory(_from:list, action=('Put','Take')):
     # middle of hotbar: 840 987
     hotbar_spacing = 65
     x, y = roblox_window_info.mid_point()
     y = roblox_window_info.bottom
 
-    def toPut(*_from):
+    def to_put(*_from):
         x = 546;y = 987 # Item 1
 
         for items in _from:
@@ -119,7 +118,7 @@ def controlInventory(_from:list, action=('Put','Take')):
                     pdi.mouseUp()
 
 
-    def toTake(*_from):
+    def to_take(*_from):
         x = 546;y = 700 # Item 1
 
         for items in _from:
@@ -148,15 +147,15 @@ def controlInventory(_from:list, action=('Put','Take')):
                     sleep(0.5)
 
 
-    if action[0] == True:
+    if action[0]:
         pdi.moveTo(x=546, y=987)
-        toPut(_from)
+        to_put(_from)
     else:
         pdi.moveTo(x=546, y=700)
-        toTake(_from)
+        to_take(_from)
 
 
-def closeInventory():
+def close_inventory():
     pdi.press('escape')
 
 
@@ -169,48 +168,57 @@ def jump(key=' ', quantity=1):
 
 # 0.75 move 1/4 em volta do player com modelo padrão
 # 0.0903 é o valor relativo (em porcentagem) do quanto o cursor irá mover.
-def cameraMove(_interval=0.4, _arrow_accurance=0.75, _mouse_accurance=0.0903, right_button=False, movement=['left', 'right']):
+def camera_move(_interval=0.4, _arrow_accurance=0.75, _mouse_accurance=0.0903, right_button=False, movement=('left', 'right')):
+    positions = str(roblox_window.rectangle())
+    left, top, right = [int(pos[1:].strip('LTR')) for pos in positions.split(',')[0:3]]
+    rx, ry = roblox_window_info.mid_point()
 
-        positions = str(roblox_window.rectangle())
-        left, top, right = [int(pos[1:].strip('LTR')) for pos in positions.split(',')[0:3]]
-        rx, ry = roblox_window_info.mid_point()
+    mouse.move(coords=(left+rx, top+ry))
 
-        mouse.move(coords=(left+rx, top+ry))
-
-        if right_button:
-            if movement == 'left':
-                fx = int((left+(rx*_mouse_accurance)))
-            else:
-                fx = int((right-(right*_mouse_accurance)))
-
-            fy = int(top+ry)
-
-            mouse.press(button='right', coords=(left+rx, fy))
-            sleep(_interval)
-            mouse.move(coords=(fx, fy))
-            mouse.release(button='right')
-            
+    if right_button:
+        if movement == 'left':
+            fx = int((left+(rx*_mouse_accurance)))
         else:
-            pdi.keyDown(movement)
-            sleep(_arrow_accurance)
-            pdi.keyUp(movement)
+            fx = int((right-(right*_mouse_accurance)))
+
+        fy = int(top+ry)
+
+        mouse.press(button='right', coords=(left+rx, fy))
+        sleep(_interval)
+        mouse.move(coords=(fx, fy))
+        mouse.release(button='right')
+        
+    else:
+        pdi.keyDown(movement)
+        sleep(_arrow_accurance)
+        pdi.keyUp(movement)
         
 # Need to add lambda for each command inside of commands (except: loop function inside of executeBoth)
-def loop(time:float, commands:list):
-  end_time = datetime.datetime.now() + datetime.timedelta(seconds=time)
-  while datetime.datetime.now() < end_time:
-        for command in commands:
-            command()
+def loop(time:float, commands:list, infinite=False):
+    while infinite:
+        end_time = datetime.datetime.now() + datetime.timedelta(seconds=time)
+        while datetime.datetime.now() < end_time:
+            for command in commands:
+                command()
+            try:
+                if keyboard.is_pressed('escape'):
+                    break
+            except Exception:
+                break
+    
+
 
 # Need to add lambda for each command inside of commands
-def executeBoth(commands:list):  
+def execute_both(commands:list):  
     Parallel(n_jobs=-1, backend='threading')(delayed(command)() for command in commands)
 
 
 
 # Test the functions
-if detectingWindow():
-    executeBoth(commands=[
-        lambda:cameraMove(movement='right'),
-        lambda:walk()
+if detecting_window():
+    loop(time=1, infinite=True, commands=[
+        lambda:execute_both(commands=[
+            lambda:use_item('left'),
+            lambda:press_key(['e', 'r']),
+        ])
     ])
